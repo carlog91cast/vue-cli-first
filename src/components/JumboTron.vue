4009bf99077a35632bb7fe10f53f7d69858554d0
bigeppo <carlog91cast@gmail.com>
2022-07-14 20:47:31 +0200

    add img to jumbotron

---
+++ src/components/JumboTron.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="jumbo-text">
+            <h2>Una nuova esperienza di guida</h2>
+            <p>Il comfort del suv, l'eleganza dello stile, il lusso nipponico, viaggiare non è mai stato cosi entusiasmante, venite a provare le nuove sospensioni idromagnetiche del nuovo geneis gv80. Fate gli svelti con le tedesche, tanto in Italia non lo vendono.</p>
+            <button>Scopri di più</button>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.container {
+    width: 100%;
+    height: 900px;
+    background-image: url(https://hd2.tudocdn.net/902314?w=1920);
+    background-size: cover;
+    position: relative;
+}
+
+div.jumbo-text h2 {
+    color: gray;
+    font-weight: 700;
+
+}
+
+div.jumbo-text p {
+    background-color: rgba(red, green, blue, alpha);
+    color: darkgrey;
+    width: 50%;
+    border-radius: 5px;
+    position: absolute;
+    bottom: 5%;
+    left: 25%;
+
+
+}
+
+button {
+    background-color:darkseagreen;
+    padding: 20px;
+    border-radius: 20px;
+}
+</style>
